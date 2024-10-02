30cdf708ef2b7bbeb19847c56bded3cf24afd37e
rs9110 <rs9110@g.rit.edu>
2024-10-01 20:37:11 -0400

    Add opengraph image

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,6 +4,7 @@ export const siteConfig = {
   name: "Rosis Sharma",
   description: "Software developer by day.",
   url: "https://rosissharma.github.io/",
+  ogImage: "https://raw.githubusercontent.com/rosissharma/rosissharma.github.io/refs/heads/main/public/opengraph-image.png",
   links: {
     github: "https://github.com/rosissharma",
     email: "mailto:rosis.sharma@outlook.com",
